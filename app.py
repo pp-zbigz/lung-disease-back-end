e69a1a24ba78587f1f7cc76c19281c9657696496
pp-zbigz <pasuk.phonsuphee@gmail.com>
2021-05-18 18:58:39 +0700

    make it better

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def upload_file():
     })
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port="80", debug=True)
+    app.run(debug=False)
+    # app.run(host="0.0.0.0", port="80", debug=False)
