f141cbf60695a5e44fa487a8a9256f9b2b974190
pp-zbigz <pasuk.phonsuphee@gmail.com>
2021-05-18 19:21:36 +0700

    update

--- app.py
+++ app.py
@@ -57,5 +57,4 @@ def upload_file():
     })
 
 if __name__ == "__main__":
-    app.run(debug=False)
-    # app.run(host="0.0.0.0", port="80", debug=False)
+    app.run(host="0.0.0.0", port="80", debug=False)
